1b0065c6014df0f0e0ab2fc2561a7af8d9670acc
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-16 16:18:09 +0900

    Initial Push
    Finished

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Welcome to Okake's Mansion</h1>
+        <p className={styles.description}>
+          We are a US/Japanese Company 
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Us!
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
