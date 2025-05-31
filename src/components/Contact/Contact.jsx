ed6954dd4d684d913df4a197d5754121f661b4bd
フォンテイン <119344403+Fontainwr@users.noreply.github.com>
2025-05-31 20:45:57 +0900

    done
    done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,6 @@ export const Contact = () => {
       <div className={styles.text}>
         <h2>Contact Info</h2>
         <p>Feel free to reach out!</p>
-              <p>Thank you</p>
           <br></br>
           <br></br>
           <br></br>
